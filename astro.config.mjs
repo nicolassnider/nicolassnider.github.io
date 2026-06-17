a5c5540d46fed5db1d0650d8c6d272e7e5c18201
Nicolás Snider <nicolas.snider@outlook.com>
2026-06-17 13:26:04 -0600

    seo updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nicolassnider.github.io',
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   i18n: {
     defaultLocale: "en",
     locales: ["en", "es"],
